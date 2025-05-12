0d77676e13597446e07287bc5698f7b33efff498
cucumberkrush <mchukwu@byupathway.edu>
2025-05-12 22:26:22 +0100

    i just added a new features that allows the user to enter their full name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("what is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("what is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.Write("what is your middle name? ");
+        string middle = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {middle}.");
     }
 }
